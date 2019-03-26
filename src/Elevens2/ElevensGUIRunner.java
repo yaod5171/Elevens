cea449b935b31fdcbf085feafede549fd86a3489
dyaom <dyaomaster@yahoo.com>
2019-03-26 08:49:04 -0700

    Reformatted Elevens2 programs

--- src/Elevens2/ElevensGUIRunner.java
+++ src/Elevens2/ElevensGUIRunner.java
@@ -1,18 +1,19 @@
 package Elevens2;
 
 /**
- * This is a class that plays the GUI version of the Elevens game.
- * See accompanying documents for a description of how Elevens is played.
+ * This is a class that plays the GUI version of the Elevens game. See
+ * accompanying documents for a description of how Elevens is played.
  */
 public class ElevensGUIRunner {
 
-	/**
-	 * Plays the GUI version of Elevens.
-	 * @param args is not used.
-	 */
-	public static void main(String[] args) {
-		Board board = new ElevensBoard();
-		CardGameGUI gui = new CardGameGUI(board);
-		gui.displayGame();
-	}
+    /**
+     * Plays the GUI version of Elevens.
+     *
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        Board board = new ElevensBoard();
+        CardGameGUI gui = new CardGameGUI(board);
+        gui.displayGame();
+    }
 }
